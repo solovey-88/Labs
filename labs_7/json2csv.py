272e0161feaab9b355ac8d9350cf01adc88e602e
solovey-88 <131691574+solovey-88@users.noreply.github.com>
2023-10-30 00:17:39 +0300

    lab_7

--- labs_7/json2csv.py
+++ labs_7/json2csv.py
@@ -1,11 +1,11 @@
-from sys import argv
+import sys
 import json
 import csv
 
 try:
-    json_name = argv[1]
+    json_name = sys.argv[1]
 except:
-    json_name = input("Введите название файла (например, sample-1.json или sample-2.json): ")
+    json_name = input("Введите название файла, например file.json: ")
 
 with open(json_name, "r", encoding="utf-8") as json_file:
     json_data = json.load(json_file)
@@ -15,9 +15,9 @@ data = list(json_data.values())[0]
 headers = data[0].keys()
 
 with open(csv_name, "w", encoding="utf-8", newline="") as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(headers)
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(headers)
     for row in data:
-        writer.writerow(row.values())
+        csv_writer.writerow(row.values())
 
-print("Конвертация", json_name, "в", csv_name, "прошла успешно!")
+print(f"Конвертация, {json_name}, в, {csv_name}, прошла успешно!")
