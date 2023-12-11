ac4473bc05cd2b7eac539bfe7b8611b70d8b3fb7
solovey-88 <131691574+solovey-88@users.noreply.github.com>
2023-12-11 09:36:21 +0300

    Update object 2.py

--- labs_2/object 2.py
+++ labs_2/object 2.py
@@ -2,9 +2,9 @@ def sierpinskiTriangle(n):
     if n == 0:
         return ["*"]
     else:
-        previous = sierpinskiTriangle(n - 1) # треугольник серпинсокго
-        space = " " * (2 ** (n - 1)) # расстояние от звездочек до краев консоли
-        result = [] # этажи треугольника
+        previous = sierpinskiTriangle(n - 1) 
+        space = " " * (2 ** (n - 1)) 
+        result = [] 
         for line in previous:
             result.append(space + line + space)
         for line in previous:
